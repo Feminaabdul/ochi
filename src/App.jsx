d0c453f4248baefed07937871c73062c280fc260
Feminaabdul <zain.3email.com@gmail.com>
2024-03-14 23:42:25 +0530

    navigation bar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-
-
+import Navbar from './Components/Navbar';
+import LandingPage from './Components/LandingPage';
 function App() {
   return (
-    <div>App</div>
+    <>
+    
+
+<div className='text-white w-full h-screen bg-zinc-900'>
+  <Navbar/>
+<LandingPage/>
+</div>
+
+    </>
   )
 }
 
